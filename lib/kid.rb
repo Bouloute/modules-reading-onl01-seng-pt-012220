d78cb1e92561f42f5c3f72120375911d49c4d300
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-10 21:39:50 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
-    extend FancyDance::ClassMethods
-    extend FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
